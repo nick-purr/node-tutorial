12d6bff98e0cf5bd0d1a8644976d00b290426d88
Nick Ellison <nickellison@purrdigital.com>
2015-01-27 22:31:36 +0000

    more changes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Hello, World!' })
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
 module.exports = router;
